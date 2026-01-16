53ff8a47f2cdd0fe07c2b7e21ccb66bd5ca15dff
bleach <142779648+b1each232425@users.noreply.github.com>
2026-01-16 09:35:37 +0800

    设定初始路由

---
+++ CompeManage/src/router/index.js
@@ -0,0 +1,17 @@
+import {createRouter,createWebHashHistory} from 'vue-router'
+const routes=[
+    {
+        path:'/',
+        redirect:'/home'
+    },
+    {
+        path:'/home',
+        component:()=>import('../views/home.vue')
+    }
+]
+
+const router=createRouter({
+    history:createWebHashHistory(),
+    routes
+})
+export default router
