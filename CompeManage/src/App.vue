88860163e00926acd56ed9afb4c8ff8f5da9ff72
bleach <142779648+b1each232425@users.noreply.github.com>
2026-01-19 11:47:42 +0800

    feat:修改首页，完善通知列表页，添加全局CSS文件

--- CompeManage/src/App.vue
+++ CompeManage/src/App.vue
@@ -11,6 +11,7 @@
     height: 100%;
     margin: 0;
     padding: 0;
+    font-family:"Helvetica Neue", Helvetica, "PingFang SC", "Microsoft YaHei", Arial, sans-serif;
 }
 #app {
     display: flex;
